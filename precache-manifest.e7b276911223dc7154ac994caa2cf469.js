b8718c1627da68a42089eb9648f9e2054b66b2a2
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2020-05-19 13:01:30 +0200

    First commit

---
+++ precache-manifest.e7b276911223dc7154ac994caa2cf469.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac23b3e6e31bc65c9fed9fad3a38918f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b11cdc5f394a83c346f",
+    "url": "/static/css/main.547c7bf4.chunk.css"
+  },
+  {
+    "revision": "affa0782947933431b87",
+    "url": "/static/js/2.1d238534.chunk.js"
+  },
+  {
+    "revision": "9b11cdc5f394a83c346f",
+    "url": "/static/js/main.636f4741.chunk.js"
+  },
+  {
+    "revision": "9e0dbbbecde63d8dbba4",
+    "url": "/static/js/runtime-main.b79f7dce.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
